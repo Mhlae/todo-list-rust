b18001cef7c6562108995b7bc30c24f58ad5f45d
Theo <theocoutocordella@gmail.com>
2026-06-22 17:30:42 -0300

    Atualização: fazndo as opções do todo-list funcionar

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,63 @@ struct Tarefa {
     concluida: bool,
 }
 
-fn user_input() -> Result<(), String>{
+fn user_input() -> String {
     let mut input = String::new();
 
     io::stdin()
-        .read_line(#mut input)
-        .expect("Erro")?;
+        .read_line(&mut input)
+        .expect("Erro");
+    input
 }
 
 fn main() {
-    let mut tarefa: Vec<Tarefa> = input();
-    let mut opção: u8 = input();
-    loop{
-        println!("1 - Adicionar");
-        println!("2 - Listar");
-        println!("3 - Sair");
+    let mut tarefas: Vec<Tarefa> = Vec::new();
+        loop{
+            println!("1 - Adicionar");
+            println!("2 - Listar");
+            println!("3 - Sair");
+            
+            let opção = user_input();
 
-        break
-    }
-    match opção.trim() {
-        "1" => println!("Adicionar");
-        "2" => println!("Listar");
-        "3" => break
-        _ => println!("erro")?;
-    }
-}
+            match opção.trim() {
+                "1" => {
+                    println!("Nome da tarefa:");
+                    let nome: String = user_input();
+                    println!("Concluída: s/n");
+                    let conclusao = user_input();
+                    let c_value: bool = match conclusao.trim() {
+                        "s" => true,
+                        "n" => false,
+                        _ => {
+                            println!("Considerado falso.");
+                            false
+                        },
+                    };
+                    let work = Tarefa{
+                        id: 0,
+                        titulo: nome,
+                        concluida: c_value,
+                    };
+                    tarefas.push(work);
+                },
+                "2" => { 
+                    if tarefas.is_empty() {
+                        println!("Nenhuma tarefa.")
+                    } else {
+                        for work in &tarefas{
+                            if work.concluida{
+                                println!("[X] {}", work.titulo);
+                            } else {
+                                println!("[ ] {}", work.titulo);
+                            }
+                        }
+                    }
+                },
+                "3" => {
+                    println!("Saindo...");
+                    break
+                },
+            _ => println!("erro"),
+            }
+        }
+        }
