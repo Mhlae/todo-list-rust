850e4217cd4ea2c31ba75d862f319a38dd17d2c5
Theo <theocoutocordella@gmail.com>
2026-06-25 22:46:09 -0300

    Primeira versão funcional por CLI

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,47 @@ fn user_input() -> String {
     input
 }
 
+fn option_1(tarefas: &mut Vec<Tarefa>) {
+    println!("Nome da tarefa:");
+    let nome: String = user_input();
+    println!("Concluída: s/n");
+    let conclusao = user_input();
+    let c_value: bool = match conclusao.trim() {
+        "s" => true,
+        "n" => false,
+        _ => {
+            println!("Considerado falso.");
+            false
+        },
+    };
+    let work = Tarefa{
+        id: 0,
+        titulo: nome,
+        concluida: c_value,
+    };
+    tarefas.push(work);
+}
+
+fn option_2(tarefas: &Vec<Tarefa>) {
+    if tarefas.is_empty() {
+        println!("Nenhuma tarefa.")
+    } else {
+        for work in tarefas{
+            if work.concluida{
+                println!("[X] {}", work.titulo);
+            } else {
+                println!("[ ] {}", work.titulo);
+            }
+        }
+    }
+}
+fn option_3() {
+    println!("Saindo...");
+}
+
 fn main() {
     let mut tarefas: Vec<Tarefa> = Vec::new();
-        loop{
+    loop{
             println!("1 - Adicionar");
             println!("2 - Listar");
             println!("3 - Sair");
@@ -26,41 +64,14 @@ fn main() {
 
             match opção.trim() {
                 "1" => {
-                    println!("Nome da tarefa:");
-                    let nome: String = user_input();
-                    println!("Concluída: s/n");
-                    let conclusao = user_input();
-                    let c_value: bool = match conclusao.trim() {
-                        "s" => true,
-                        "n" => false,
-                        _ => {
-                            println!("Considerado falso.");
-                            false
-                        },
-                    };
-                    let work = Tarefa{
-                        id: 0,
-                        titulo: nome,
-                        concluida: c_value,
-                    };
-                    tarefas.push(work);
+                   option_1(&mut tarefas);
                 },
                 "2" => { 
-                    if tarefas.is_empty() {
-                        println!("Nenhuma tarefa.")
-                    } else {
-                        for work in &tarefas{
-                            if work.concluida{
-                                println!("[X] {}", work.titulo);
-                            } else {
-                                println!("[ ] {}", work.titulo);
-                            }
-                        }
-                    }
+                    option_2(&tarefas);
                 },
                 "3" => {
-                    println!("Saindo...");
-                    break
+                    option_3();
+                    break;
                 },
             _ => println!("erro"),
             }
